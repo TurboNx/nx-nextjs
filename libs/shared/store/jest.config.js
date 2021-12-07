63bdf92affa3357ad7f5ece9a712486db0a2ded7
gxkai <30716965+gxkai@users.noreply.github.com>
2021-12-07 09:11:37 +0800

    menu

---
+++ libs/shared/store/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'shared-store',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/store',
+};
